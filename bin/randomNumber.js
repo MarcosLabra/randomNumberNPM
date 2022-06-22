4ec2ee395f74128b97d9b63830bd9a725177a4fc
MarcosLabra <mlabra.dev@gmail.com>
2022-06-22 10:45:36 -0300

    corrijo nan  npx

--- bin/randomNumber.js
+++ bin/randomNumber.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
 const random = require('../src/index.js');
-
-console.log(random(Number(process.argv[2]),Number(process.argv[3])));
+if(process.argv[2] !== undefined && process.argv[3] !== undefined) {
+    console.log(`el numero aleatorio entre ${process.argv[2]} y ${process.argv[3]} es: `)
+    console.log(random(Number(process.argv[2]),Number(process.argv[3])));
+} else {
+    console.log(`el numero aleatorio entre 0 y 1000 (por defecto) es: `)
+    console.log(random());
+}
